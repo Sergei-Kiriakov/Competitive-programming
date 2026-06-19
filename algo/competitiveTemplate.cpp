bbd90f78d3fbc1b0ea984ea6cde1c4baa563ce43
Sergei Kiriakov <serik030603@gmail.com>
2026-06-19 10:05:05 +0300

    Update competitiveTemplate.cpp to current style (matches the §1 template)

--- algo/competitiveTemplate.cpp
+++ algo/competitiveTemplate.cpp
@@ -1,36 +1,54 @@
-// #pragma GCC optimize("Ofast,unroll-loops")
-// #pragma GCC target("avx,avx2,fma")
-
-#include <bits/stdc++.h>
-
-#define int long long
-#define endl "\n"
-#define all(x) (x).begin(), (x).end()
+#include <functional>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <list>
+#include <math.h>
+#include <cstdlib>
+#include <fstream>
+#include <queue>
+#include <set>
+#include <vector>
+#include <stack>
+#include <bitset>
+#include <random>
+#include <chrono>
+#include <cassert>
+#include <memory>
 
 using namespace std;
 
-constexpr int mod = 1e9 + 7;
+#define int long long
 
-void solve()
-{ 
-    return; 
+const int MOD = 1e9 + 7;
+
+int solve (int n, vector<int> a) {
+    int ans = 0;
+
+    return ans;
+}
+
+void input() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    auto ans = solve(n, a);
+    cout << ans << endl;
 }
 
 int32_t main()
 {
-    // ofstream cout("");
-    // ifstream cin("");
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    cout << fixed;
-    cout.precision(7);
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
     int tt = 1;
     cin >> tt;
-    while(tt-->0)
-    {
-        solve();
-    } 
-}
 
-/*
-*/  
+    while (tt-->0)
+    {
+        input();
+    }
+}
